ed87c6a70e850a0ea403c03243641b7bda2a728b
julianaflima <juli.faccio@gmail.com>
2023-01-08 07:27:55 +0530

    Create operate function

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -20,3 +20,22 @@ function divide(a, b) {
 	return (a / b);
 }
 
+
+function operate(number1, number2, operation) {
+		switch (operation) {
+			case '+':
+				return add(number1, number2);
+				break;
+			case '-': 
+				return subtract(number1, number2);
+				break;
+			case '*':
+				return multiply(number1, number2);
+				break;
+			case '/': 
+				return divide(number1, number2);
+				break;
+		}
+		
+		return result;
+}
